cd119bda626574a9c01cafc87d217ce86f3b0821
Nelson Cespedes <anodyne.dev@gmail.com>
2026-05-08 14:56:52 +1000

    FEAT(icons): Add new icons for app manifest and update layout references 🎨✨

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,11 @@ export function generateMetadata(): Metadata {
     icons: {
       icon: [
         { url: '/favicon.svg', type: 'image/svg+xml' },
+        { url: '/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+        { url: '/icon-512x512.png', sizes: '512x512', type: 'image/png' },
       ],
       apple: [
+        { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
         { url: '/apple-touch-icon.svg', type: 'image/svg+xml' },
       ],
     },
